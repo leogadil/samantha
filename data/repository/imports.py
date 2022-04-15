9a67125c79abbf3f75b137c1de3ed1d7f2fa20db
LeoooooGadil <leogadil04@gmail.com>
2022-04-16 00:24:10 +0800

    update imported

--- data/repository/imports.py
+++ data/repository/imports.py
@@ -3,6 +3,8 @@ import os
 
 from .action import Action, ActionManager
 from .intent import Recognizer, Trainer
+from .smalltalk import SmallTalk
+
 from .helper import import_actions
 
 import_actions()
